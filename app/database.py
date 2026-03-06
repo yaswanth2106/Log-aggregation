3c03b44c8547fb14c618c09ae8b793b6b51caa7a
yaswanth2106 <yashwanthforever21@gmail.com>
2026-03-07 02:40:37 +0530

    remove sqlite check_same_thread for postgres

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
